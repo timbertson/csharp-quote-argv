f43f8f760389b0fbc7159efde5090d7963f4d998
Tim Cuthbertson <tim@gfxmonk.net>
2014-04-23 22:52:12 +1000

    add copyright header

--- QuoteArguments.cs
+++ QuoteArguments.cs
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2003-2014 by
+ *   Peter Astrand <astrand@lysator.liu.se> (python's subprocess module)
+ *   Tim Cuthbertson <tim@gfxmonk.net> (C# port of this code)
+ *
+ * ======================= The MIT License ==============================
+ *
+ * Permission to use, copy, modify, and distribute this software and
+ * its associated documentation for any purpose and without fee is
+ * hereby granted, provided that the above copyright notice appears in
+ * all copies, and that both that copyright notice and this permission
+ * notice appear in supporting documentation, and that the name of the
+ * author not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior
+ * permission.
+ *
+ * THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
+ * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 using System;
 using System.Text;
 using System.Collections;
@@ -5,7 +30,6 @@ using System.Collections;
 public class QuoteArguments
 {
 	public static string Quote(IList args) {
-		// adopted from python's subprocess.list2cmdline function
 		StringBuilder sb = new System.Text.StringBuilder();
 		foreach (string arg in args) {
 			int backslashes = 0;
